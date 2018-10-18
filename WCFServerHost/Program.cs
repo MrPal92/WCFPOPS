73302b1577c68675d4a103ade9a21c37afc16e3f
Sumit Pal <mr.pal92@gmail.com>
2018-10-18 22:45:12 +0530

    Added config of serviceHost

--- WCFServerHost/Program.cs
+++ WCFServerHost/Program.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ServiceModel;
 namespace WCFServerHost
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            ServiceHost serviceHost = new ServiceHost(typeof(WCFPOPS.POPS));
+            serviceHost.Open();
+            Console.WriteLine("Service Is Running...");
+            Console.WriteLine("Press Enter to stop.");
+            Console.ReadLine();
+            serviceHost.Close();
         }
     }
 }
